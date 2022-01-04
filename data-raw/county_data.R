ccd5cfcf83cdd62977770f3f11dbf7bb6ab3b246
Carl Frederick <Carl.Frederick@dpi.wi.gov>
2022-01-04 11:11:09 -0600

    updating data file to 2020

--- data-raw/county_data.R
+++ data-raw/county_data.R
@@ -1,4 +1,4 @@
 ## code to prepare `county_data` dataset goes here
 county_data <- readr::read_csv("data-raw/wi_dph_regions.csv", col_types = "ccncc")
 
-usethis::use_data(county_data)
+usethis::use_data(county_data, overwrite = TRUE)
